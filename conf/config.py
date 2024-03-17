b513050c521f8d8421f0caaf50152fe38395874c
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2024-03-17 11:45:58 +0000

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -11,7 +11,5 @@ class Settings(BaseSettings):
     PASSWORD: str
     NEWS_API_KEY: str
 
-    model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")  # noqa
-
 
 settings = Settings()
