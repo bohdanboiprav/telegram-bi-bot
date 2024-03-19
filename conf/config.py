2493093ecc63dadc1eb31f55307f4248934c0f5b
Catalin Bondari <catalin.bondari2@mail.dcu.ie>
2024-03-19 22:45:26 +0000

    comments added

--- conf/config.py
+++ conf/config.py
@@ -1,7 +1,9 @@
+# This file contains the configuration settings for the bot.
 from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
+# Class to store the configuration settings
 class Settings(BaseSettings):
     BOT_TOKEN: str
     ENDPOINT: str
@@ -12,4 +14,9 @@ class Settings(BaseSettings):
     NEWS_API_KEY: str
 
 
-settings = Settings()
+settings = Settings()  # Creating an instance of the Settings class
+
+"""In the above code snippet, we have defined a Settings class that inherits from the BaseSettings class provided 
+by the pydantic_settings library. This class contains the configuration settings required for the bot, such as the 
+bot token, database connection details, and the news API key. The settings object is an instance of the Settings class 
+that can be used to access these configuration settings in other parts of the code."""
--- database/db.py
+++ database/db.py
@@ -1,3 +1,4 @@
+# This file contains the database connection and query execution functions
 from contextlib import asynccontextmanager
 
 import aiopg
@@ -5,6 +6,7 @@ import aiopg
 from conf.config import settings
 
 
+# Function to establish a connection with the database
 @asynccontextmanager
 async def database_connection():
     try:
@@ -19,6 +21,7 @@ async def database_connection():
         pass
 
 
+# Function to execute a select query and return the results
 async def async_select_one(custom_query: str):
     async with database_connection() as conn:
         await conn.execute(custom_query)
@@ -38,22 +41,7 @@ async def async_insert(custom_query: str):
         await conn.execute(custom_query)
 
 
-"""
-async def async_insert(custom_query: str):
-    session = boto3.Session(aws_access_key_id=settings.AWS_SERVER_PUBLIC_KEY,
-                            aws_secret_access_key=settings.AWS_SERVER_SECRET_KEY, region_name=settings.AWS_REGION)
-    client = session.client('rds')
-
-    token = client.generate_db_auth_token(DBHostname=settings.ENDPOINT, Port=settings.PORT, DBUsername=settings.DB_USER,
-                                          Region=settings.REGION)
-
-    try:
-        async with aiopg.connect(host=settings.ENDPOINT, port=settings.PORT, database=settings.DBNAME,
-                                 user=settings.DB_USER, password=settings.PASSWORD,
-                                 sslrootcert="SSLCERTIFICATE") as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(custom_query)
-                await conn.commit()
-    except Exception as e:
-        print("Database connection failed due to {}".format(e))
-"""
+"""In the above code snippet, we have defined a context manager called database_connection that establishes a connection 
+with the database using the aiopg library. The async_select_one and async_select_all functions execute select queries and 
+return the results. The async_insert function is used to execute insert queries. These functions are used to interact with 
+the database in the services/scrapper.py script."""
